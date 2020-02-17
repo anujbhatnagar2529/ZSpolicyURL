9449a815f10d867d03e86654782bcfecf3f5f43f
Anuj BHATNAGAR <61127682+anujbhatnagar2529@users.noreply.github.com>
2020-02-17 23:09:25 +0530

    namingPolicy

--- namingPolicy.sh
+++ namingPolicy.sh
@@ -4,19 +4,21 @@
 epochstamp=$(date +%s)
 
 #Generate uniq string
-randomString=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+randomString=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1 | awk '{print tolower($0)}')
 
 helpFunction()
 {
+
+   echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
    echo ""
    echo -e "\t Possible ways to call the utility..."
    echo ""
    echo -e "\t ---------------------------------------"
    echo -e "\t For Staging and Production Environment-"
-   echo -e "\t Usage: $0 -a [stg|prd] -b [devid]"
+   echo -e "\t Usage: $0 -a [stg|prd] -b [apac|emea|amer] -c [devid]"
    echo -e "\t "
    echo -e "\t For Client Environment-"
-   echo -e "\t Usage: $0 -a [cmr] -b [cmrid]"
+   echo -e "\t Usage: $0 -a [cmr] -b [apac|emea|amer] -c [cmrid]"
    echo -e "\t ---------------------------------------"
    echo ""
    echo -e "\t-a can be stg or prd or cmr, to idenity link is for prod or stage or customer environment"
@@ -24,24 +26,26 @@ helpFunction()
    echo ""
    echo -e "\tFinal results would be"
    echo -e "\t Step 1:"
-   echo -e "\t\tproduction|staging|customer.developerID|customerID.randomString"
+   echo -e "\t\t[production|staging|customer].[APAC|EMEA|AMER].[developerID|customerID].[randomString]"
    echo ""
-   echo -e "\t\t prd.dx1234.$randomString"
+   echo -e "\t\t prd.apac.d1234.$randomString"
    echo -e "\t\t OR "
-   echo -e "\t\t cmr.cx1234.$randomString"
+   echo -e "\t\t cmr.apac.c1234.$randomString"
    echo ""
    echo -e "\t Step 2:"
    echo -e "\t\tStoring the Data for Future Reference"
    echo -e "\t\tFile - /tmp/urlinfo.txt"
-   
+   echo ""
+   echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
    exit 1 # Exit script after printing help
 }
 
-while getopts "a:b:" opt
+while getopts "a:b:c:" opt
 do
    case "$opt" in
       a ) parameterA="$OPTARG" ;;
       b ) parameterB="$OPTARG" ;;
+      c ) parameterC="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
@@ -51,42 +55,50 @@ echo "URL String Generation  Process Started at $(date)"
 # Print helpFunction in case parameters are empty
 if [ -z "$parameterA" ];
 then
-   echo "Type of environment is not provided, the parameter -a is empty";
+   echo "Type of environment [stg or prd or cmr] is not provided, the parameter -a is empty";
    helpFunction
 fi
 
+# Print helpFunction in case parameters are empty
 if [ -z "$parameterB" ];
 then
-   echo "Type of id is not provided, the parameter -b is empty";
+   echo "Type of region [apac or emea or amer] is not provided, the parameter -b is empty";
    helpFunction
 fi
-# Begin script in case all parameters are correct
-echo "$parameterA.$parameterB"  
 
-# Check whether first character is denoting customer or developer
-echo "${parameterB::2}" | egrep -c cx
-if [ $? -eq 0 ];
+if [ -z "$parameterC" ];
 then
-	echo "Processing for Developer Environment - $parameterA.$parameterB" 
+   echo "Type of id .i.e. [developerID or customerID] is not provided, the parameter -c is empty";
+   helpFunction
 fi
 
-echo "${parameterB::2}" | egrep -c dx
-if [ $? -eq 0 ];
-	echo "Processing for Customer Environment - $parameterA.$parameterB" 
-else
-	echo "Incorrect Prefix input provided for $parameterB" 
-	echo "Exiting..."
-	helpFunction
+
+#checking provided input for $parameterA
+chkparamA1=$(echo $parameterA | egrep -i "stg|prd")
+if [ -z $chkparamA1 ];
+then
+	chkparamA2=$(echo $parameterA | egrep -i "cmr")
+	if [ -z $chkparamA2 ];
+	then
+		"Correct value not provided, retry and select one from [stg|prd|cmr]"
+	fi
+fi
+
+#checking provided input for $parameterB
+chkparamB=$(echo $parameterB | egrep -i "apac|emea|amer")
+if [ -z $chkparamB ];
+then
+	"Correct value not provided, retry and select one from [apac|emea|amer]"
 fi
 
 echo "Generated string is..."
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "$parameterA.$parameterB.$randomString"
+echo "$parameterA.$parameterB.$parameterC.$randomString"
 echo "++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 echo "------- Storing the Data for Future Reference ----------"
 touch /tmp/urlinfo.txt
-echo "$parameterA|$parameterB|$randomString|$epochstamp >> /tmp/urlinfo.txt"
+echo "$parameterA|$parameterB|$parameterC|$randomString|$epochstamp" >> /tmp/urlinfo.txt
 echo "Current entry - $(tail -1 /tmp/urlinfo.txt)"
 echo ""
 echo "All entries - $(tail -1 /tmp/urlinfo.txt)"
